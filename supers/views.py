2f480b684946c1b11862a6da71023a4b3ca66e66
John Parks <jcparks1984@gmail.com>
2022-02-18 15:00:17 -0500

    Added Project Directios To Readme

--- supers/views.py
+++ supers/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import supersSerializer
+from Super_Type.serializers import SuperTypeSerializer
 from .models import Supers
 
 
